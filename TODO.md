f2c01a981f47b0d187145a80168843fc493ba5bc
RebekahJ <rebekahjs17@gmail.com>
2014-09-19 18:56:09 +0100

    UPDATE
    Add to tests, TODO notes

--- TODO.md
+++ TODO.md
@@ -1,4 +1,6 @@
 TODO
 ====
 
+- Decide treatment of negative and 0 values
+- Add tests for gmean output and error finding
 
--- test/test.js
+++ test/test.js
@@ -23,6 +23,33 @@ describe( 'flow-gmean', function tests() {
 		expect( flowFactory ).to.be.a( 'function' );
 	});
 
-	it( 'should do something' );
+	it( 'should provide a method to get the initial accumulator value', function test() {
+		var tStream = flowFactory();
+		expect( tStream.value() ).to.be.a( 'number' );
+	});
+
+	it( 'should provide a method to set the initial accumulator value', function test() {
+		var tStream = flowFactory();
+		tStream.value( 5 );
+		assert.strictEqual( tStream.value(), 5 );
+	});
+
+	it( 'should not allow a non-numeric initial accumulator value', function test() {
+		var tStream = flowFactory();
+		
+		expect( badValue( '5' ) ).to.throw( Error );
+		expect( badValue( [] ) ).to.throw( Error );
+		expect( badValue( {} ) ).to.throw( Error );
+		expect( badValue( null ) ).to.throw( Error );
+		expect( badValue( undefined ) ).to.throw( Error );
+		expect( badValue( NaN ) ).to.throw( Error );
+		expect( badValue( function(){} ) ).to.throw( Error );
+
+		function badValue( value ) {
+			return function() {
+				tStream.value( value );
+			};
+		}
+	});
 
 });
